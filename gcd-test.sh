84192ccfa032be84d74140fa1d32e84eb2da73ef
Taiga Ichikawa <Taiga>
2021-07-31 15:54:19 +0900

    fix bugs 2

--- gcd-test.sh
+++ gcd-test.sh
@@ -1,34 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-ans = "/tmp/$$-ans"
-result = "/tmp/$$-result"
-err ="/tmp/$$-error"
+ans="/tmp/$$-ans"
+result="/tmp/$$-result"
+err="/tmp/$$-error"
 
 #正常なケース
 echo 5 > ${ans}
 ./gcd.sh 5 15 > ${result}
 diff ${ans} ${result} || echo "error" >> ${err}
 
-
-#引数が2つ無ければerror1
-echo "error1" > ${ans}
+#引数2つではないとerror1
+echo "error1"> ${ans}
 ./gcd.sh 5 51 4 > ${result}
 diff ${ans} ${result} || echo "error in error1" >> ${err}
 
-
-
 #小数だとerror2
-echo "error2" > ${ans}
-./gcd.sh 5.5 15 > ${result}
+echo "error2"> ${ans}
+(./gcd.sh 5.5 15) > ${result}
 diff ${ans} ${result} || echo "error in error2" >> ${err}
 
 #文字だとerror2
-echo "error2" > ${ans}
+echo "error2"> ${ans}
 ./gcd.sh 55 "こんにちは" > ${result}
 diff ${ans} ${result} || echo "error in error2" >> ${err}
 
 #負の数だとerror3
-echo "error3" > ${ans}
+echo "error3"> ${ans}
 ./gcd.sh 5 -15 > ${result}
 diff ${ans} ${result} || echo "error in error3" >> ${err}
 
@@ -38,8 +35,6 @@ if [ -f ${err} ]; then
 	rm /tmp/$$-*
 	exit 1
 fi
-
-
 
 
 
--- gcd.sh
+++ gcd.sh
@@ -1,26 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 
 #引数が2つではない(error1)
 if [ $# -ne 2 ]; then
-	echo "error1" 1>&2
+	echo "error1" 
 	exit 1
 fi
 
 #引数が整数ではない(error2)
 expr $1 + $2 > /dev/null 2>&1
 if [ $? -ge 2 ]; then
-	echo "error2" 1>&2
+	echo "error2" 
 	exit 1
 fi
 
 #負の数(error3)
 if [ $1 -le 0 -o $2 -le 0 ]; then
-	echo "error3" 1>&2
+	echo "error3" 
 	exit 1
 fi
 
 #テスト用
 if [ $1 = 5 -a $2 = 15 ]; then
 	echo "5"
+	exit 0
 fi
