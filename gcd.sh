5b64ebce61df7a9ed77e5e482f1367451cfd820b
Taiga Ichikawa <Taiga>
2021-07-31 17:36:08 +0900

    バグを修正した

--- gcd.sh
+++ gcd.sh
@@ -20,8 +20,22 @@ if [ $1 -le 0 -o $2 -le 0 ]; then
 	exit 1
 fi
 
-#テスト用
-if [ $1 = 5 -a $2 = 15 ]; then
-	echo "5"
-	exit 0
+
+#最大公約数を計算
+
+m=$1
+if [ $2 -lt $m ]
+then
+	m=$2
 fi
+while [ $m -ne 0 ]
+do
+	x=`expr $1 % $m`
+	y=`expr $2 % $m`
+	if [ $x -eq 0 -a $y -eq 0 ]
+	then
+		echo $m
+		break
+	fi
+	m=`expr $m - 1`
+done
