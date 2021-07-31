2988caabe7c3a049055b38420132ba4ede59b483
Taiga Ichikawa <Taiga>
2021-07-31 14:52:46 +0900

    fix bugs

--- gcd-test.sh
+++ gcd-test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ans = "/tmp/$$-ans"
 result = "/tmp/$$-result"
@@ -16,19 +16,20 @@ echo "error1" > ${ans}
 diff ${ans} ${result} || echo "error in error1" >> ${err}
 
 
-#負の数だとerror2
+
+#小数だとerror2
 echo "error2" > ${ans}
-./gcd.sh 5 -15 > ${result}
+./gcd.sh 5.5 15 > ${result}
 diff ${ans} ${result} || echo "error in error2" >> ${err}
 
-#小数だとerror3
-echo "error3" > ${ans}
-./gcd.sh 5.5 15 > ${result}
-diff ${ans} ${result} || echo "error in error3" >> ${err}
-
-#文字だとerror3
-echo "error3" > ${ans}
+#文字だとerror2
+echo "error2" > ${ans}
 ./gcd.sh 55 "こんにちは" > ${result}
+diff ${ans} ${result} || echo "error in error2" >> ${err}
+
+#負の数だとerror3
+echo "error3" > ${ans}
+./gcd.sh 5 -15 > ${result}
 diff ${ans} ${result} || echo "error in error3" >> ${err}
 
 
@@ -36,7 +37,9 @@ if [ -f ${err} ]; then
 	cat ${err}
 	rm /tmp/$$-*
 	exit 1
-}
+fi
+
+
 
 
 
